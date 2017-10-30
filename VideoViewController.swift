41e26ece84a9f4de37c016d6deeb6ffdaaf2ef4b
Jesus Quezada <maj3st1@gmail.com>
2017-10-30 18:36:13 -0500

    JQ

---
+++ VideoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  VideoViewController.swift
+//  MyVideo
+//
+//  Created by Jesus Quezada on 10/30/17.
+//  Copyright © 2017 Jesus Quezada. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class VideoViewController: AVPlayerViewController {
+    
+}
